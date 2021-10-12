5b1d5e930cf5cac85d37bbfda69e7d586eea92dc
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-12 21:29:36 +0800

    Create ReviewAdapter.kt
    Buat ReviewAdapter. Masukkan Implementasi dari turunan dan buat construktor

---
+++ app/src/main/java/com/ryfazrin/restaurantreview/ReviewAdapter.kt
@@ -0,0 +1,23 @@
+package com.ryfazrin.restaurantreview
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ReviewAdapter(private val listReview: List<String>) : RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ReviewAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
