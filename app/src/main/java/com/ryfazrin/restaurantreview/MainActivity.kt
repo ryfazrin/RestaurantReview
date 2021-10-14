4fea92336e8389538b4c4453501b5479412ed0c6
Muhammad Pazrin Andreanor <riansera515@gmail.com>
2021-10-14 10:01:31 +0800

    Update MainActivity.kt
    Kemudian inisialisasi ViewModel dan observe setiap data dan panggil fungsi yang dibutuhkan dari MainActivity

--- app/src/main/java/com/ryfazrin/restaurantreview/MainActivity.kt
+++ app/src/main/java/com/ryfazrin/restaurantreview/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
@@ -25,15 +26,26 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val mainViewModel = ViewModelProvider(this,ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            setRestaurantData(restaurant)
+        })
+
         val layoutManager = LinearLayoutManager(this)
         binding.rvReview.layoutManager = layoutManager
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
 
-        findRestaurant()
+        mainViewModel.listReview.observe(this, { consumerReviews ->
+            setReviewData(consumerReviews)
+        })
+
+        mainViewModel.isLoading.observe(this, {
+            showLoading(it)
+        })
 
         binding.btnSend.setOnClickListener { view ->
-            postReview(binding.edReview.text.toString())
+            mainViewModel.postReview(binding.edReview.text.toString())
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
